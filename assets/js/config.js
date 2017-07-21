d1168932d4b139c0f4ede16d8fc5357853a4a221
cazorla19 <michael.chinkov@gmail.com>
2017-07-21 14:41:17 +0300

    Append the function to get Pagerduty token from URL parameter

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,22 @@
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
+
+var token = getUrlParameter('token')
+
 var pdjs_settings = {
-  subdomain: "pdt-ryan",
-  token: "zinNPMQB4xn3zNJX9ZWR",
+  subdomain: "flysoft",
+  token: token,
   refresh: 10000,
 }
